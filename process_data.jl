7e4672713a02a6ba8048b64ef8ad4ebcc8d458a1
Alexandre Heiden <alexandreheiden@hotmail.com>
2018-10-09 11:20:23 -0300

    coding

--- process_data.jl
+++ process_data.jl
@@ -1,15 +1,25 @@
-using CSV
-using Statistics
-using DataFrames
+using CSV, Statistics, DataFrames, Devectorize
 
-# asset's expected return over time t = [1, T]
-function expected_return(asset, T)
+# asset's EXPECTED return over time t = [1, T]
+function expected_return(assets, T)
 	rj = 0.0
 	for i in 2:T
 		rj += (asset[i] - asset[i-1])
 	end
 	μj = (1 / T+1) * rj
 	return μj
+end
+
+# asset's EXACT return over time t = [1, T]
+function exact_return(assets, T)
+	return asset[T] - asset[1]
+end
+
+function portfolio_variance(Rj, μj)
+    acc = 0
+    for i in 1:length(Rj)
+    	acc += 
+    end
 end
 
 file = "gerdau.csv"
@@ -22,7 +32,11 @@ rename!(df, Symbol("Adj Close") => Symbol("Adj_Close"))
 
 asset = df.Adj_Close
 
-μj = expected_return(asset, T)
+μj = expected_return(assets, T)
+Rj = exact_return(assets, T)
+c = portfolio_variance(Rj, μj)
 
 println("T: ", T)
-println(μj)
+println("Exact: ", Rj)
+println("Expected: ", μj)
+println("Portfolio variance: ", c)
