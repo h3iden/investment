74f16dd650077c3468849e502759445c824aad85
Alexandre Heiden <alexandreheiden@hotmail.com>
2018-10-09 00:05:50 -0300

    expected asset return

--- process_data.jl
+++ process_data.jl
@@ -1,12 +1,28 @@
 using CSV
 using Statistics
+using DataFrames
 
-file = "test.csv"
+# asset's expected return over time t = [1, T]
+function expected_return(asset, T)
+	rj = 0.0
+	for i in 2:T
+		rj += (asset[i] - asset[i-1])
+	end
+	μj = (1 / T+1) * rj
+	return μj
+end
+
+file = "gerdau.csv"
 df = CSV.read(file)
-println(df.Adj_Close)
 
 T = CSV.nrow(df)
 
-while true
-    print(df.Adj_Close[rand(1:T)])
-end
+# replaces the " " in the column name with a "_" for direct access
+rename!(df, Symbol("Adj Close") => Symbol("Adj_Close"))
+
+asset = df.Adj_Close
+
+μj = expected_return(asset, T)
+
+println("T: ", T)
+println(μj)
