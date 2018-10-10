b1bb2f47b75eb9d422e78b8049d44a7aabba5af0
Alexandre Heiden <alexandreheiden@hotmail.com>
2018-10-10 18:28:12 -0300

    variance

--- porra.jl
+++ porra.jl
@@ -1,12 +1,15 @@
-function f()
-	r = rand(0:0, 100)
-	a, b = rand(1:1, 100), rand(1:1, 100)
-	for i in 1:10000000
-		@. r += a + b # broadcast the + operator
-		# r += a + b
-	end
-	return r
-end
+# function f()
+# 	r = rand(0:0, 100)
+# 	a, b = rand(1:1, 100), rand(1:1, 100)
+# 	for i in 1:10000000
+# 		@. r += a + b # broadcast the + operator
+# 		# r += a + b
+# 	end
+# 	return r
+# end
 
-@time r = f()
-println(r)
+# @time r = f()
+# println(r)
+
+x = readdir("./assets/")
+println(x)
--- process_data.jl
+++ process_data.jl
@@ -15,25 +15,43 @@ function exact_return(assets, T)
 	return asset[T] - asset[1]
 end
 
-function portfolio_variance(Rj, μj)
-    acc = 0
-    for i in 1:length(Rj)
-    	acc += 
-    end
+function asset_pair_variance(Ri, Rj, μi, μj)
+    return (Ri - μi) * (Rj - μj)
 end
 
-file = "gerdau.csv"
-df = CSV.read(file)
+function portfolio_variance(x, R, μ)
+    acc = 0.0
+    for i in 1:length(x, R, μ)
+    	for j in 1:length(x)
+    		σij = asset_pair_variance(R[i], μ[i], R[j], μ[j])
+    		acc += (σ * x[i] * x[j])
+    	end
+    end
+    return acc
+end
 
-T = CSV.nrow(df)
+T, assets, μ, R = [], [], [], []
+files = readdir("./assets/")
+id = 1
+for file in files
+	df = CSV.read(file)
+	# replaces the " " in the column name with a "_" for direct access
+	rename!(df, Symbol("Adj Close") => Symbol("Adj_Close"))
+	append!(T, CSV.nrow(df))
+	push!(assets, df.Adj_Close)
+	append(μ, expected_return(assets[id], T[id]))
+	append!(R, exact_return(assets[id], T[id]))
+	id += 1
+end
 
-# replaces the " " in the column name with a "_" for direct access
-rename!(df, Symbol("Adj Close") => Symbol("Adj_Close"))
 
-asset = df.Adj_Close
+# file = "assets/gerdau.csv"
 
-μj = expected_return(assets, T)
-Rj = exact_return(assets, T)
+# T = CSV.nrow(df)
+
+
+# asset = df.Adj_Close
+
 c = portfolio_variance(Rj, μj)
 
 println("T: ", T)
